1010106076a9e828c98da9c7bd59c62d64b5778d
Vers3Dynamics <93827718+topherchris420@users.noreply.github.com>
2026-03-10 21:41:50 -0400

    Delete james_library-main directory

--- james_library-main/graph_r1_engine/graphr1/__init__.py
+++
@@ -1,3 +0,0 @@
-from .graphr1 import GraphR1 as GraphR1
-from .graphr1 import QueryParam as QueryParam
-
--- james_library-main/show_error.py
+++
@@ -1,5 +0,0 @@
-import json
-
-with open('ruff_errors.json') as f:
-    for e in json.load(f):
-        print(f"{e['filename']}:{e['location']['row']} - {e['message']}")
